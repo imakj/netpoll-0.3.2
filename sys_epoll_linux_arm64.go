e389aac8320ebf84e2cc83021cba7f074c9e9d7a
makj <mkj199102022@163.com>
2023-07-05 21:35:18 +0800

    123

--- sys_epoll_linux_arm64.go
+++ sys_epoll_linux_arm64.go
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build linux && loong64 && riscv64
-// +build linux,loong64,riscv64
-
 package netpoll
 
 import (
